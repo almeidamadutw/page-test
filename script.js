47b72d414a23623108938e6058b4c98028c097d1
madu <164823888+almeidamadutw@users.noreply.github.com>
2026-03-20 21:51:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const revealItems = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.14
+});
+
+revealItems.forEach((item) => observer.observe(item));
